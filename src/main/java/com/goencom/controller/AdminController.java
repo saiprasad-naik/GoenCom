bc686e6d31aac5c0ba497fe98929b2e488297b9a
vaibhav <kalalvaibhav@gmail.com>
2022-06-23 21:05:05 +0530

    admin - auction house list is being displayed

--- src/main/java/com/goencom/controller/AdminController.java
+++ src/main/java/com/goencom/controller/AdminController.java
@@ -1,24 +1,49 @@
 package com.goencom.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.goencom.dao.UserRepository;
+import com.goencom.entities.Item;
+import com.goencom.entities.User;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	@Autowired
+	private UserRepository userRepository;
+	
 	@GetMapping("/")
 	public String home() {
 		return "admin-dashboard";
 	}
 	
-	@GetMapping("/manage-users")
-	public String manageUsers() {
+	@GetMapping("/manage-users/{page}")
+	public String manageUsers(@PathVariable("page") Integer page, Model model) {
+		Pageable pageable = PageRequest.of(page, 4);
+		Page<User> users = userRepository.findAllUsers(pageable);
+		model.addAttribute("users", users);
+		model.addAttribute("currentPage", page);
+		model.addAttribute("totalPages", users.getTotalPages());
 		return "admin-manage-users";
 	}
 	
-	@GetMapping("/manage-auction-house")
-	public String manageAdmin() {
+	@GetMapping("/manage-auction-house/{page}")
+	public String manageAdmin(@PathVariable("page") Integer page, Model model) {
+		Pageable pageable = PageRequest.of(page, 4);
+		Page<User> users = userRepository.findAllAuctionHouses(pageable);
+		model.addAttribute("users", users);
+		model.addAttribute("currentPage", page);
+		model.addAttribute("totalPages", users.getTotalPages());
 		return "admin-manage-auction-house";
 	}
 	
--- src/main/java/com/goencom/dao/UserRepository.java
+++ src/main/java/com/goencom/dao/UserRepository.java
@@ -1,5 +1,7 @@
 package com.goencom.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +11,10 @@ import com.goencom.entities.User;
 public interface UserRepository extends JpaRepository<User, Integer> {
 	@Query("select u from User u where u.email = :email")
 	public User getUserByEmail(@Param("email")String email);
+	
+	@Query("select u from User u where u.role = 'ROLE_USER'")
+	public Page<User> findAllUsers(Pageable pageable);
+	
+	@Query("select u from User u where u.role = 'ROLE_AUCTIONEER'")
+	public Page<User> findAllAuctionHouses(Pageable pageable);
 }
